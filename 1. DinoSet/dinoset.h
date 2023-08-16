1c8628fd3515dcd5168113010b827e421e7267a4
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-08-16 23:09:14 +0000

    Create initial files for DinoSet lab

---
+++ 1. DinoSet/dinoset.h
@@ -0,0 +1,14 @@
+#pragma once
+
+const int total_dinos = 8;
+
+enum class DinoId {
+    velociraptor,        // 0
+    stegosaurus,         // 1
+    tyrannosaurus,       // 2
+    procompsognathus,    // 3
+    triceratops,         // 4
+    pachycephalosaurus,  // 5
+    parasaurolophus,     // 6
+    pteranodon           // 7
+};
---
+++ 1. DinoSet/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "dinoset.h"
+
+int main()
+{
+    return 0;
+}
