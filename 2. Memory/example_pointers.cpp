3d65704d347729f8095a43014177679275a580ab
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-08-23 23:41:18 +0000

    Add code to demonstrate pass by value vs using pointers

--- 2. Memory/example_pointers.cpp
+++ 2. Memory/example_pointers.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+void swap1(int x, int y)
+{
+    int t = x;
+    x = y;
+    y = t;
+}
+
+void swap2(int* p, int* q)
+{
+    int t = *p;
+    *p = *q;
+    *q = t;
+}
+
 int main()
 {
     int x = 5;
@@ -16,6 +30,17 @@ int main()
     std::cout << "pp   = " << pp << "\n";
     std::cout << "*pp  = " << *pp << "\n";
     std::cout << "**pp = " << **pp << "\n";
+
+    int a = 5;
+    int b = 8;
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    swap1(a, b);
+    std::cout << "a = " << a << ", b = " << b << "\n\n";
+
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    swap2(&a, &b);
+    std::cout << "a = " << a << ", b = " << b << "\n";
+
 
     return 0;
 }
