3df2c99d6062705b068f74c848eb063bb936a427
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-09-01 22:58:36 +0000

    Add code to implement linked list operations

--- 2. Memory/simple_linked_list.cpp
+++ 2. Memory/simple_linked_list.cpp
@@ -5,12 +5,104 @@ struct Node {
     Node* next;
 };
 
-int main()
+// Complexity: O(1)
+void insert(Node*& list, int value)
 {
     Node* p = new Node;
-    p->value = 42;
-    p->next = nullptr;
-
-    Node* list;
+    p->value = value;
+    p->next = list;
     list = p;
+}
+
+// Complexity: O(N)
+void free(Node*& list)
+{
+    Node* p = list;
+    while (p) {
+        Node* next = p->next;
+        delete p;
+        p = next;
+    }
+    list = nullptr;
+}
+
+// Complexity: O(N)
+void display(Node* list)
+{
+    Node* p = list;
+    while (p) {
+        std::cout << p->value << " ";
+        p = p->next;
+    }
+    std::cout << "\n";
+}
+
+// Complexity: O(N)
+int length(Node* list)
+{
+    Node* p = list;
+    int result = 0;
+    while (p) {
+        ++result;
+        p = p->next;
+    }
+    return result;
+}
+
+
+// Complexity: O(1)
+void drop(Node*& list)
+{
+    if (list) {
+        Node* p = list;
+        list = list->next;
+        delete p;
+    }
+}
+
+// Complexity: O(N)
+bool remove(Node*& list, int value)
+{
+    Node* p = list;
+    Node* prev = nullptr;
+    while (p) {
+        if (p->value == value) {
+            if (prev) {
+                prev->next = p->next;
+            } else {
+                list = p->next;
+            }
+            delete p;
+            return true;
+        }
+        prev = p;
+        p = p->next;
+    }
+    return false;
+}
+
+int main()
+{
+    Node* list = nullptr;
+    std::cout << "length(list) = " << length(list) << "\n";
+    display(list);
+    insert(list, 42);
+    display(list);
+    insert(list, 23);
+    display(list);
+    insert(list, 16);
+    display(list);
+    insert(list, 15);
+    display(list);
+    std::cout << "length(list) = " << length(list) << "\n";
+    drop(list);
+    drop(list);
+    display(list);
+    std::cout << "length(list) = " << length(list) << "\n";
+    std::cout << "remove(list, 42) = " << remove(list, 42) << "\n";
+    std::cout << "remove(list, 108) = " << remove(list, 108) << "\n";
+    std::cout << "remove(list, 23) = " << remove(list, 23) << "\n";
+    display(list);
+    free(list);
+    display(list);
 }
